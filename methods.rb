41f121a37ccd537a1ce2fc28ade19309d88d6a2e
Sandy Zhang <sandyzhang142@gmail.com>
2022-12-12 13:01:57 -0500

    commit tree d52f88fba1923d5a0704def350cc70d2ffb6ef24

--- methods.rb
+++ methods.rb
@@ -1,1 +1,43 @@
 # Your code here!
+# function greetProgrammer() {
+#   console.log("Hello, programmer!");
+# }
+def greet_programmer
+    puts "Hello, programmer!"
+
+end 
+
+# function greet(name) {
+#   console.log(`Hello, ${name}!`);
+# }
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+# function greetWithDefault(name = "programmer") {
+#   console.log(`Hello, ${name}!`);
+# }
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+# function add(num1, num2) {
+#   return num1 + num2;
+# }
+
+def add(num1, num2) 
+    num1 + num2
+end
+
+# function halve(number) {
+#   if (typeof number !== "number") return null;
+
+#   return number / 2;
+# }
+
+def halve(num)
+    return nil unless num.class == Integer
+    num / 2 
+end
